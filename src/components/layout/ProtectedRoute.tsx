b9e6bb39e2b5290802fafdabb1c9fe8bb108cdca
Rahad-Ullah <myemail.abc20@gmail.com>
2024-07-07 14:48:57 +0600

    Protected Route implemented

---
+++ src/components/layout/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+import { useSelector } from "react-redux";
+import { useCurrentToken } from "../../redux/features/auth/authSlice";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const token = useSelector(useCurrentToken);
+
+  if (!token) {
+    return <Navigate to={"/login"} replace={true} />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
