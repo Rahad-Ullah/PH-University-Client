6a4cc3c25c277ce188774202730f0070ce2b3ad5
Rahad-Ullah <myemail.abc20@gmail.com>
2024-08-30 10:53:42 +0600

    Academic Semester creation successfully implemented

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,19 +1,21 @@
-import { useGetAllSemestersQuery } from "../../../redux/features/academicSemester/academicSemesterApi";
-
+import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
 
 const AcademicSemester = () => {
-    const {data} = useGetAllSemestersQuery(undefined)
-    console.log(data?.data);
-    const semesters = data?.data
-    
-    return (
-        <div>
-            <h1 className="text-4xl font-bold">This is Academic Semester </h1>
-            {
-                semesters && semesters.map(item => <h1 key={item._id} className="text-3xl">{item.name} {item.startMonth} {item.year}</h1>)
-            }
-        </div>
-    );
+  const { data } = useGetAllSemestersQuery(undefined);
+  console.log(data?.data);
+  const semesters = data?.data;
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">This is Academic Semester </h1>
+      {semesters &&
+        semesters.map((item) => (
+          <h1 key={item._id} className="text-3xl">
+            {item.name} {item.startMonth} {item.year}
+          </h1>
+        ))}
+    </div>
+  );
 };
 
 export default AcademicSemester;
